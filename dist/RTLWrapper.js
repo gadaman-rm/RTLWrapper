3692e1f41c387d78c2f59168f015d2773ae0d4f3
gadaman <randm2001@gmail.com>
2023-08-23 17:08:34 +0330

    Add default

--- dist/RTLWrapper.js
+++ dist/RTLWrapper.js
@@ -7,13 +7,13 @@ function RTLWrapper({
     if (children && children.props && children.props.children) {
       if (children.props.children.length !== undefined) {
         const processedChild = /*#__PURE__*/React.cloneElement(children, {
-          className: `${children.props.className ? children.props.className : ''} ${dir}`,
+          className: `${children.props.className ? children.props.className : ""} ${dir}`,
           children: React.Children.map(children.props.children, process)
         });
         return processedChild;
       } else {
         const processedChild = /*#__PURE__*/React.cloneElement(children, {
-          className: `${children.props.className ? children.props.className : ''} ${dir}`,
+          className: `${children.props.className ? children.props.className : ""} ${dir}`,
           children: process(children.props.children)
         });
         return processedChild;
@@ -24,4 +24,8 @@ function RTLWrapper({
   }
   return /*#__PURE__*/React.createElement(React.Fragment, null, process(children));
 }
+RTLWrapper.defaultProps = {
+  dir: "ltr" // Default value for the dir prop
+};
+
 export default RTLWrapper;
--- src/RTLWrapper.js
+++ src/RTLWrapper.js
@@ -1,29 +1,35 @@
 import React from "react";
 
 function RTLWrapper({ children, dir }) {
-    function process(children) {
-        if (children && children.props && children.props.children) {
-            if (children.props.children.length !== undefined) {
-                const processedChild = React.cloneElement(children, {
-                    className: `${children.props.className ? children.props.className : ''} ${dir}`, children: React.Children.map(children.props.children,
-                        process)
-                });
-                return processedChild;
-            }
-            else {
-                const processedChild = React.cloneElement(children, {
-                    className: `${children.props.className ? children.props.className : ''} ${dir}`,
-                    children: process(children.props.children)
-                });
-                return processedChild; 
-            }
-        }
-        else {
-            return children;
-        }
+  function process(children) {
+    if (children && children.props && children.props.children) {
+      if (children.props.children.length !== undefined) {
+        const processedChild = React.cloneElement(children, {
+          className: `${
+            children.props.className ? children.props.className : ""
+          } ${dir}`,
+          children: React.Children.map(children.props.children, process),
+        });
+        return processedChild;
+      } else {
+        const processedChild = React.cloneElement(children, {
+          className: `${
+            children.props.className ? children.props.className : ""
+          } ${dir}`,
+          children: process(children.props.children),
+        });
+        return processedChild;
+      }
+    } else {
+      return children;
     }
+  }
 
-    return <React.Fragment>{process(children)}</React.Fragment>;
+  return <React.Fragment>{process(children)}</React.Fragment>;
 }
+
+RTLWrapper.defaultProps = {
+  dir: "ltr", // Default value for the dir prop
+};
 
 export default RTLWrapper;
